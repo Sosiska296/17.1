a7dff8049445f0098d542752f941844456fc07f7
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-05-30 22:02:55 +0500

    Добавьте файлы проекта.

---
+++ WpfApp17/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+namespace WpfApp17;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private DispatcherTimer timer;
+    private int counter;
+    private bool counting;
+    public MainWindow()
+    {
+        InitializeComponent();
+        timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromSeconds(0.5);
+        timer.Tick += (sender, args) =>
+        {
+            Поле.Text = counter.ToString();
+            counter++;
+        };
+    }
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (counting)
+        {
+            timer.Stop();
+            counting = false;
+        }
+        else
+        {
+            timer.Start();
+            counting = true;
+        }
+    }
+
+}
